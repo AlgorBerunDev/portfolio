6868b53c30b992964eaf7eff582789440890ee47
Doston <algorberun@gmail.com>
2021-10-28 07:38:10 +0500

    Created SessionResource

---
+++ app/app/Http/Resources/SessionResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SessionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'access_token' => $this->access_token,
+            'refresh_token' => $this->refresh_token,
+        ];
+    }
+}
